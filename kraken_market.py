62d634f8ca66c3f87a72348a336839b444618ec3
jbies121 <15245768+jbies121@users.noreply.github.com>
2021-08-28 12:42:23 -0500

    interval to string

--- kraken_market.py
+++ kraken_market.py
@@ -5,7 +5,7 @@ import json
 interval = 60
 # Get OHLC data of an asset since a specified Unix epoch time
 def kraken_price(asset_index,time_unix,interval):
-    price_check_uri = 'https://api.kraken.com/0/public/OHLC?pair='+asset_index+'&since='+str(time_unix)+'&interval='+interval
+    price_check_uri = 'https://api.kraken.com/0/public/OHLC?pair='+asset_index+'&since='+str(time_unix)+'&interval='+str(interval)
     price = requests.get(price_check_uri)
     price = json.loads(price.content.decode())
     return price
