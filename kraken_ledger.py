94deb306a4ad4ed23563444f4df006074c730233
jbies121 <15245768+jbies121@users.noreply.github.com>
2021-09-27 12:40:28 -0500

    Replace stake_transactions with ledger script

---
+++ kraken_ledger.py
@@ -0,0 +1,65 @@
+import time
+import datetime
+from openpyxl import load_workbook
+from kraken_auth import get_kraken_signature,kraken_request
+from kraken_market import kraken_price
+from secrets import api_key,api_sec
+
+def update_ledger(wb,ws,last_row,kraken_time_u,kraken_time,transaction,amount):
+    next_row = last_row + 1
+    # get asset price at kraken_time
+    price = kraken_price(asset_index,kraken_time_u - 3600,60)
+    price = price['result'][asset_index][00][4]
+    # add new ledger entry
+    ws.cell(row = next_row, column = 1).value = kraken_time
+    ws.cell(row = next_row, column = 2).value = transaction
+    ws.cell(row = next_row, column = 3).value = asset
+    ws.cell(row = next_row, column = 4).value = amount
+    ws.cell(row = next_row, column = 5).value = price
+    wb.save('Crypto Ledger.xlsx')
+    print('Updated Ledger:',kraken_time)
+
+
+def stake_update(resp):
+    # Read most recent staking activity
+    kraken_time_u = resp['result'][0]['time']
+    kraken_time = datetime.datetime.fromtimestamp(kraken_time_u)
+    # Read last entry in ledger
+    wb = load_workbook(filename = 'Crypto Ledger.xlsx')
+    ws = wb.active
+    last_row = ws.max_row
+    last_time = ws.cell(row = last_row, column = 1).value
+    # compare most recent stake entry timestamp to ledger
+    if kraken_time > last_time:
+        ahead = 1
+        while ahead:
+            kraken_time_u = resp['result'][ahead]['time']
+            kraken_time = datetime.datetime.fromtimestamp(kraken_time_u)
+            if kraken_time > last_time:
+                ahead = ahead + 1
+
+            else:
+                ahead = ahead - 1
+                # Set fields
+                kraken_time_u = resp['result'][ahead]['time']
+                kraken_time = datetime.datetime.fromtimestamp(kraken_time_u)
+                transaction = resp['result'][ahead]['type'].upper()
+                asset = str(resp['result'][ahead]['asset'])[:-2]
+                asset_index = asset+'USD'
+                amount = resp['result'][ahead]['amount']
+
+                update_ledger(wb,ws,last_row,kraken_time_u,kraken_time,transaction,amount)
+                # Update last row and time for next iteration
+                last_row = ws.max_row
+                last_time = ws.cell(row = last_row, column = 1).value
+
+    else:
+        print('No update needed.')
+
+# Construct the request and print the result
+resp = kraken_request('/0/private/Staking/Transactions', {
+    "nonce": str(int(1000*time.time()))
+}, api_key, api_sec)
+
+stake_update(resp.json())
+print(resp.json())
